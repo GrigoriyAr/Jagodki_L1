f0e69cc170a34323ff4c0cfc5cd9d4c4e236833e
GrigoriyAr <107112000+GrigoriyAr@users.noreply.github.com>
2023-02-27 21:17:57 +0300

    Update main.go

--- task14/main.go
+++ task14/main.go
@@ -1,3 +1,10 @@
+/*
+Задание:
+Разработать программу, которая в рантайме способна определить тип переменной:
+int, string, bool, channel из переменной типа interface{}.
+*/
+
+
 package main
 
 import "fmt" //"reflect"
